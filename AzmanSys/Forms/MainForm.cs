8d55585fc59bec81604549a801a6fe1b664d53fa
Jordan Adamson <JAA18@outlook.com>
2019-02-08 17:01:21 +0000

    Initial Connect

---
+++ AzmanSys/Forms/MainForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AzmanSys
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnManageCustomers_Click(object sender, EventArgs e)
+        {
+            Close(); // closes the current "MainForm" form
+            CustomersForm custForm = new CustomersForm();
+            custForm.Show();
+        }
+
+        private void btnManageFlights_Click(object sender, EventArgs e)
+        {
+            Close(); // closes the current "MainForm" form
+            FlightsForm flightsForm = new FlightsForm();
+            flightsForm.Show();
+        }
+
+        private void btnManageBookings_Click(object sender, EventArgs e)
+        {
+            Close(); // closes the current "MainForm" form
+            BookingsForm bookingsForm = new BookingsForm();
+            bookingsForm.Show();
+          
+        }
+    }
+}
